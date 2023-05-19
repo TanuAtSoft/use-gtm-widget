838159d3c230fe1a819c55575186f95190aeeb40
webdev.sp <webdev.sp@amtexsystems.com>
2023-05-19 15:51:38 +0530

    worked on classname

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <h1>Testing App for GTM</h1>
+      <div className='product-page-container'>
+        this is product page container
+      </div>
     </div>
   );
 }
