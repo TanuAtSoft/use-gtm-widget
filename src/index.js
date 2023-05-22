ae1881a30ef57f2d8f2633d001f3201d0f554dac
webdev.sp <webdev.sp@amtexsystems.com>
2023-05-22 17:41:27 +0530

    removed g-id

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import TagManager from 'react-gtm-module';
+// import TagManager from 'react-gtm-module';
 
-const tagManagerArgs ={
-  gtmId: 'G-1TXB674LL3'
-}
+// const tagManagerArgs ={
+//   gtmId: 'G-1TXB674LL3'
+// }
 
-TagManager.initialize(tagManagerArgs)
+// TagManager.initialize(tagManagerArgs)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
