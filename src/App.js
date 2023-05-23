220c1d823de9feb96c676bba65c5e8943cffdc50
webdev.sp <webdev.sp@amtexsystems.com>
2023-05-23 12:59:56 +0530

    added text and p tag

--- src/App.js
+++ src/App.js
@@ -1,14 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Testing App for GTM</h1>
-      <div className='product-page-container' onClick={()=> alert("clicked")}>
-        this is product page container
+      <div className="product-page-container" onClick={() => alert("clicked")}>
+       <p className="test-p"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Nibh cras pulvinar
+        mattis nunc sed. Facilisi cras fermentum odio eu feugiat pretium nibh
+        ipsum consequat. Eget nulla facilisi etiam dignissim. Massa eget egestas
+        purus viverra accumsan in. Volutpat ac tincidunt vitae semper quis
+        lectus nulla at. Leo vel orci porta non. Tincidunt id aliquet risus
+        feugiat in ante. Sed vulputate odio ut enim blandit volutpat maecenas
+        volutpat blandit. Augue mauris augue neque gravida in fermentum. Morbi
+        tristique senectus et netus et malesuada fames ac. Non blandit massa
+        enim nec dui nunc mattis enim ut. Ullamcorper morbi tincidunt ornare
+        massa eget egestas purus viverra. Nisl pretium fusce id velit ut tortor
+        pretium viverra. Pellentesque sit amet porttitor eget dolor morbi non
+        arcu risus. Id donec ultrices tincidunt arcu non sodales neque sodales.
+        Arcu dictum varius duis at consectetur lorem donec massa. Cursus turpis
+        massa tincidunt dui ut ornare lectus sit amet. Ac turpis egestas
+        maecenas pharetra convallis. Duis at tellus at urna condimentum mattis
+        pellentesque id. Maecenas accumsan lacus vel facilisis volutpat. Egestas
+        erat imperdiet sed euismod nisi porta lorem mollis. Non pulvinar neque
+        laoreet suspendisse. Quis varius quam quisque id diam. Amet mattis
+        vulputate enim nulla. At imperdiet dui accumsan sit amet nulla. Donec
+        ultrices tincidunt arcu non sodales neque sodales. Non diam phasellus
+        vestibulum lorem sed risus ultricies tristique nulla. Tellus id interdum
+        velit laoreet id. Aliquam ut porttitor leo a diam. Commodo viverra
+        maecenas accumsan lacus vel. Facilisis gravida neque convallis a cras
+        semper auctor. Vivamus arcu felis bibendum ut tristique et egestas. Enim
+        neque volutpat ac tincidunt vitae semper quis lectus nulla. Elementum eu
+        facilisis sed odio morbi quis. Euismod lacinia at quis risus. Nibh
+        tortor id aliquet lectus proin nibh nisl. Tempus egestas sed sed risus
+        pretium quam vulputate dignissim suspendisse. Iaculis urna id volutpat
+        lacus. Diam ut venenatis tellus in metus vulputate. Viverra justo nec
+        ultrices dui sapien.
+        </p>
+        <p> Egestas diam in arcu cursus euismod quis. Dictum at
+        tempor commodo ullamcorper a lacus vestibulum. Lacus suspendisse
+        faucibus interdum posuere lorem ipsum dolor sit. Porta lorem mollis
+        aliquam ut porttitor leo. Sodales ut eu sem integer vitae. Aliquam purus
+        sit amet luctus. Vitae proin sagittis nisl rhoncus. Aliquet enim tortor
+        at auctor urna nunc id. Vitae auctor eu augue ut lectus arcu. Urna
+        condimentum mattis pellentesque id nibh tortor. Nullam non nisi est sit
+        amet facilisis. Amet porttitor eget dolor morbi non arcu. Magna ac
+        placerat vestibulum lectus mauris ultrices eros in cursus. Arcu non
+        sodales neque sodales ut etiam sit. Mollis aliquam ut porttitor leo a
+        diam sollicitudin tempor. Fermentum dui faucibus in ornare. Leo integer
+        malesuada nunc vel risus. Amet venenatis urna cursus eget nunc. Non diam
+        phasellus vestibulum lorem sed risus ultricies tristique nulla. Volutpat
+        consequat mauris nunc congue nisi vitae suscipit. Malesuada fames ac
+        turpis egestas integer eget aliquet nibh praesent. Nulla posuere
+        sollicitudin aliquam ultrices sagittis. Leo a diam sollicitudin tempor
+        id. Orci dapibus ultrices in iaculis nunc sed augue lacus viverra. Augue
+        mauris augue neque gravida in fermentum et. Maecenas volutpat blandit
+        aliquam etiam erat. Viverra mauris in aliquam sem fringilla. Enim neque
+        volutpat ac tincidunt vitae. Accumsan sit amet nulla facilisi morbi
+        tempus iaculis. Et egestas quis ipsum suspendisse ultrices gravida
+        dictum. Phasellus faucibus scelerisque eleifend donec pretium vulputate
+        sapien nec sagittis. Mauris pharetra et ultrices neque ornare aenean
+        euismod. Metus dictum at tempor commodo ullamcorper. Sapien pellentesque
+        habitant morbi tristique senectus. Fames ac turpis egestas maecenas
+        pharetra convallis. Enim sed faucibus turpis in eu mi bibendum. Diam
+        vulputate ut pharetra sit amet.
+        </p>
       </div>
-      <button onClick={()=> alert("clicked test Click")}>Test Click</button>
+
     </div>
   );
 }
